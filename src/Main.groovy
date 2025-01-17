de0b5bdc903bbf1f3d1b41192f84c91584bd3d04
Martínez <sergio.martinez@adesso.es>
2025-01-17 12:25:36 +0100

    Script updated

--- src/Main.groovy
+++ src/Main.groovy
@@ -27,6 +27,18 @@ sql.execute("DELETE FROM orders WHERE user_id = ?", [2])
 // DELETE operation: Delete the user
 sql.execute("DELETE FROM users WHERE id = ?", [2])
 
+// DELETE operation: Delete the user alice.smith@example.com if are more than 1
+def count = sql.firstRow("SELECT COUNT(*) AS count FROM users WHERE email = ?", ['alice.smith@example.com']).count
+if (count > 1) {
+    sql.execute("DELETE FROM users WHERE email = ?", ['alice.smith@example.com'])
+    println "Deleted user alice.smith@example.com as there were more than one"
+    def newResult = sql.rows("SELECT * FROM users")
+    println "Users updated:"
+    newResult.each { row ->
+        println "ID: ${row.id}, Name: ${row.name}, Email: ${row.email}"
+    }
+}
+
 // INNER JOIN operation: Retrieve orders with user details
 println "\nOrders and Users (INNER JOIN):"
 def joinResult = sql.rows("""
@@ -36,6 +48,17 @@ INNER JOIN users u ON o.user_id = u.id
 """)
 joinResult.each { row ->
     println "Order ID: ${row.order_id}, Name: ${row.name}, Product: ${row.product}, Amount: ${row.amount}"
+}
+
+//// RIGHT JOIN operation: Retrieve all users and their orders (even if no orders exist for users)
+println "\nUsers and Orders (RIGHT JOIN):"
+def rightJoinResult = sql.rows("""
+SELECT u.id AS user_id, u.name, u.email, o.product, o.amount
+FROM orders o
+RIGHT JOIN users u ON o.user_id = u.id
+""")
+rightJoinResult.each { row ->
+    println "User ID: ${row.user_id}, Name: ${row.name}, Product: ${row.product ?: 'No Product'}, Amount: ${row.amount ?: 'N/A'}"
 }
 
 // Close the database connection
